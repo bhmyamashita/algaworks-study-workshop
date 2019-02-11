3bab369bdaa04fea012ff7c2041c2f33ae6d7ebf
bhmyamashita <bhmyamashita@gmail.com>
2019-02-10 22:21:21 -0300

    rename

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/comercial-ui'));
+app.use(express.static(__dirname + '/dist/parking-project'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/comercial-ui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/parking-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
